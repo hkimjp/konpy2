6b0c96b95bb00bb131d1b91a8036cedd379882c3
hkimjp <hkim@hkim.jp>
2025-11-30 11:09:51 +0900

    color self?

--- dev/user.clj
+++ dev/user.clj
@@ -1,7 +1,9 @@
 (ns user
   (:require
    [clj-reload.core :as reload]
+   [clojure.java.io :as io]
    [taoensso.telemere :as tel]
+   [hkimjp.datascript :as ds]
    [hkimjp.konpy2.system :refer [start-system stop-system restart-system]]))
 
 (tel/set-min-level! :debug)
@@ -24,4 +26,29 @@
 
 ;; (reload/reload)
 
+; ----------------
 
+(ds/conn?)
+
+(ds/pull @ds/conn ['*] 100)
+
+; why this can't?
+(ds/q @ds/conn '[:find ?e
+                 :where
+                 [?e]])
+
+(ds/qq '[:find (count ?e)
+         :where [?e]])
+
+(def problems (ds/qq '[:find ?week ?num ?problem
+                       :keys week  num  problem
+                       :where
+                       [?e :week ?week]
+                       [?e :num ?num]
+                       [?e :problem ?problem]]))
+
+(sort-by :week problems)
+
+(sort-by (fn [x] (:week x)) problems)
+
+(spit (io/file "konpy.txt") (sort-by (fn [x] [(:week x) (:num x)]) problems))
